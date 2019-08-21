44b4a2160a5d54ddbfcba24bc2653e7dffc7aca3
13cramerr <13cramerr@gmail.com>
2019-08-20 22:34:53 -0400

    add intro to variables and data types

--- Giraffe/app.py
+++ Giraffe/app.py
@@ -1,2 +1,26 @@
-# hello world
-print("Hello World")
+# # hello world
+# print("Hello World")
+#
+# # drawing shapes
+# print("   /|")
+# print("  / |")
+# print(" /  |")
+# print("/___|")
+#
+# # Variables
+# # story without vars
+# print("There once was a man named George, ")
+# print("he was 70 years old. ")
+# print("He really liked the name George, ")
+# print("but didn't like being 70.")
+
+# # story with vars
+# character_name = "John"
+# character_age = 35
+#
+# print("There once was a man named " + character_name + ", ")
+# print("he was " + str(character_age) + " years old. ")
+#
+# character_name = "Mike"
+# print("He really liked the name " + character_name + ", ")
+# print("but didn't like being " + str(character_age) + ".")
